54be1f5f2bd968853a2a5f767a97c80f45ca276f
Anna <anna.gloriole@gmail.com>
2022-10-17 11:35:13 +0300

    input array

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@ int arLength = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("input max string length");
 int maxLength = int.Parse(Console.ReadLine()!);
+
+string[] InputArray(int Length)
+{
+    Console.WriteLine("input array elements");
+    string[] arrInt = new string[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        arrInt[i] = Console.ReadLine()!;
+    }
+    return arrInt;
+}
